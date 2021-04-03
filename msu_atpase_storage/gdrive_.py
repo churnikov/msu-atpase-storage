634e13b16517c4258252e97e3112fa374503114d
Nikita Churikov <nikita@chur.ru>
2021-04-03 15:21:38 +0300

    NEW upload files though bot to gdrive NEW docstingspr

--- msu_atpase_storage/gdrive_.py
+++ msu_atpase_storage/gdrive_.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+from pathlib import Path
 from typing import Sequence
 
 from pydrive.auth import GoogleAuth
@@ -9,25 +9,43 @@ from msu_atpase_storage.types_ import GDriveFile
 
 
 class GDrive:
+    """
+    Wrapper for pydrive.
+    """
+
     def __init__(self):
         gauth = GoogleAuth(settings_file=settings.gdrive_settings_path)
         gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
         self.drive = GoogleDrive(gauth)
 
-    def upload_content(self, content: BytesIO, file_name: str) -> GDriveFile:
-        # TODO обрабатывать tmp файл
+    def upload_file(self, path: Path, file_id: str) -> GDriveFile:
+        """
+        Upload file from a `path`. File will be saved as `file_id.ext`.
+        We will take extension from `path`
+
+        :param path: path of a file
+        :param file_id: string id of a file
+        :return: Meta info of a file saved in gdrive
+        """
+        file_name = file_id + "".join(path.suffixes)
         file = self.drive.CreateFile({"parents": [{"id": settings.gdrive_folder_id}], "title": file_name})
 
-        file.SetContentString(content.read().decode())
+        file.SetContentFile(path)
         file.Upload()
         file.InsertPermission({"type": "anyone", "value": "anyone", "role": "reader"})
         fobj = GDriveFile(id_=file["id"], filename=file_name, link=file["alternateLink"])
         return fobj
 
-    def remove_file(self, file_id: str):
+    def remove_file(self, file_id: str) -> None:
+        """
+        Delete file from gdrive folder.
+
+        :param file_id: gdrive id of a file (not ours)
+        """
         file = self.drive.CreateFile({"id": file_id})
         file.Delete()
 
-    def list_content(self) -> Sequence[str]:
+    def list_files(self) -> Sequence[str]:
+        """List all files in a folder."""
         file_list = self.drive.ListFile({"q": f"'{settings.gdrive_folder_id}' in parents and trashed=false"}).GetList()
         return file_list  # Has title and id fields
--- tests/test_gdrive.py
+++ tests/test_gdrive.py
@@ -1,9 +1,15 @@
-from io import BytesIO
+import tempfile
+from pathlib import Path
 
 from msu_atpase_storage.gdrive_ import GDrive
 
 
 def test_create_and_delete_file():
     drive = GDrive()
-    file = drive.upload_content(BytesIO(b"rstrst"), "test_file.txt")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmp_file = Path(tmpdirname) / "tmp_file.txt"
+        with tmp_file.open("w") as f:
+            f.write("pup")
+
+        file = drive.upload_file(tmp_file, "____01")
     drive.remove_file(file.id_)
