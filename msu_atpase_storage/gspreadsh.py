4d8b1287b2bb9b4115d0d2461e55d9a8367783de
Nikita Churikov <nikita@chur.ru>
2021-04-04 23:00:04 +0300

    UPD messages text UPD reverse rows added

--- msu_atpase_storage/gspreadsh.py
+++ msu_atpase_storage/gspreadsh.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import gspread
 
 from msu_atpase_storage.config import settings
@@ -18,10 +16,6 @@ class GSheet:
         values_list = self.sh.col_values(1)
         return len(values_list) + 1
 
-    def add_row(self, data: GSheetRow, row_id: Optional[int] = None):
+    def add_row(self, data: GSheetRow):
         """Add new row to google spreadsheet"""
-        next_row_id = self.get_next_free_row_id() if row_id is None else row_id
-        col_list = self.sh.range(f"A{next_row_id}:F{next_row_id}")
-        for cl, value in zip(col_list, data):
-            cl.value = value
-        self.sh.update_cells(col_list)
+        self.sh.insert_row(values=list(data), index=2)
