259b66e042c7d3e579242e6b6ec893f665cc5d8c
Nikita Churikov <8545082+churnikov@users.noreply.github.com>
2021-04-04 20:36:48 +0300

    NEW Dockerfile (#9)

--- msu_atpase_storage/config.py
+++ msu_atpase_storage/config.py
@@ -7,9 +7,6 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     tg_token: str = Field(..., env="TG_TOKEN")
-    aws_access_key_id: str = Field(..., env="AWSAccessKeyId")
-    aws_secret_key: str = Field(..., env="AWSSecretKey")
-    aws_busket: str = Field(..., env="AWSBusket")
     id_size: int = Field(..., env="ID_Size")
     mongo_host: str = Field("localhost", env="MONGO_HOST")
     mongo_port: str = Field(27017, env="MONGO_PORT")
